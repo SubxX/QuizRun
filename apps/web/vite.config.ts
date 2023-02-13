010bb8bb378841db2922c4e2baeaf4529a7460a5
Subham <subhambhattacharya700@gmail.com>
2023-02-13 19:51:44 +0530

    build(workflo): deploy workflow changes

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import reactPreset from 'vite-preset-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    reactPreset({ removeDevtoolsInProd: true, injectReact: true }),
-  ],
+export default defineConfig(({ mode }: any) => {
+  const env = loadEnv(mode, process.cwd())
 
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  }
+  return ({
+    base: mode === 'production' ? env.VITE_BASE_URL : '/',
+    plugins: [
+      tsconfigPaths(),
+      reactPreset({ removeDevtoolsInProd: true, injectReact: true }),
+    ],
+
+    esbuild: {
+      logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    }
+  })
 });
